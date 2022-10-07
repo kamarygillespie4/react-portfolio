1d78735043bb609885ad52b8ae7e96a84e5f01d3
Kamary <gillespiekamary@gmail.com>
2022-10-07 13:03:37 -0600

    created basic footer template that will be styled later

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Navbar } from "./components/Navbar";
-import Projects from "./components/Projects";
+import { Projects } from "./components/Projects";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Projects />
+      <Footer />
     </>
   );
 }
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,5 +1,25 @@
 import React from "react";
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-light text-center text-lg-start fixed-bottom">
+      <div className="text-center p-3">
+        <a className="text-dark" href="https://github.com/kamarygillespie4">
+          GITHUB LOGO
+        </a>
+        <a
+          className="text-dark"
+          href="https://www.linkedin.com/in/kamary-gillespie/"
+        >
+          LINKED IN LOGO
+        </a>
+        <a
+          className="text-dark"
+          href="https://stackoverflow.com/users/20186894/kamary-gillespie?tab=profile"
+        >
+          STACK OVERFLOW LOGO
+        </a>
+      </div>
+    </footer>
+  );
 };
