d6de2112a8a5c7ee6ad2e5ad4a86c9a3e9ff3d70
Kamary <gillespiekamary@gmail.com>
2022-10-09 12:29:17 -0600

    favbar and footer fully templated, need to get routing done and do styling

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,5 +1,30 @@
 import React from "react";
 
-function Footer() {}
+const styles = {
+  footer: {
+    position: "fixed",
+    bottom: "0",
+    left: "0",
+    width: "100%",
+    height: "4rem",
+    background: "#D0DAEE",
+  },
+};
+
+function Footer() {
+  return (
+    <footer
+      className="bg-light text-center text-lg-start"
+      style={styles.footer}
+    >
+      <div className="text-center p-3">
+        © 2020 Copyright:
+        <a className="text-dark" href="https://mdbootstrap.com/">
+          MDBootstrap.com
+        </a>
+      </div>
+    </footer>
+  );
+}
 
 export default Footer;
--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,5 +1,25 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
-function Navigation() {}
+function Navigation() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">KAMARY GILLESPIE</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">ABOUT ME</Nav.Link>
+            <Nav.Link href="#link">MY PORTFOLIO</Nav.Link>
+            <Nav.Link href="#link">MY RESUME</Nav.Link>
+            <Nav.Link href="#link">CONTACT ME</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
 
 export default Navigation;
