fdaabf75222ead2f704265a928bb47280884a5e5
Kamary <gillespiekamary@gmail.com>
2022-10-09 10:51:27 -0600

    rebuilt app

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
-import Navigation from "./components/Navbar";
-import Background from "./components/Background";
-import Footer from "./components/Footer";
-import About from "./components/About";
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
+function App() {
   return (
-    <>
-      <Background />
-      <Navigation />
-      <About />
-      <Footer />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
-//
+export default App;
