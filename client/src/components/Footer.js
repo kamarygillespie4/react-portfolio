66b1de3e4ad0e653d0a0e1860f854d0906be7f73
Kamary <gillespiekamary@gmail.com>
2022-10-09 19:19:59 -0600

    styled footer and provided links

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,12 +2,24 @@ import React from "react";
 
 const styles = {
   footer: {
-    position: "static",
+    position: "relative",
     bottom: "0",
     left: "0",
-    width: "100ww",
+    width: "100vw",
     height: "5vh",
-    background: "#D0DAEE",
+  },
+  div: {
+    background: "#38a3af",
+  },
+  icons: {
+    height: "4vh",
+    width: "auto",
+    "margin-left": "2.5%",
+    "margin-right": "2.5%",
+  },
+  link: {
+    "padding-left": "2%",
+    "padding-right": "2%",
   },
 };
 
@@ -17,15 +29,45 @@ function Footer() {
       className="bg-light text-center text-lg-start"
       style={styles.footer}
     >
-      <div className="text-center p-3">
-        <a className="text-dark" href="https://mdbootstrap.com/">
-          MDBootstrap.com
+      <div className="text-center p-3" style={styles.div}>
+        <a
+          className="text-dark"
+          href="https://mdbootstrap.com/"
+          target="_blank"
+          style={styles.link}
+          rel="noreferrer"
+        >
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+            alt="github icon"
+            style={styles.icons}
+          />
         </a>
-        <a className="text-dark" href="https://mdbootstrap.com/">
-          MDBootstrap.com
+        <a
+          className="text-dark"
+          href="https://www.linkedin.com/in/kamary-gillespie/"
+          target="_blank"
+          style={styles.link}
+          rel="noreferrer"
+        >
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/61/61109.png"
+            alt="github icon"
+            style={styles.icons}
+          />
         </a>
-        <a className="text-dark" href="https://mdbootstrap.com/">
-          MDBootstrap.com
+        <a
+          className="text-dark"
+          href="https://mdbootstrap.com/"
+          target="_blank"
+          style={styles.link}
+          rel="noreferrer"
+        >
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/2111/2111640.png"
+            alt="github icon"
+            style={styles.icons}
+          />
         </a>
       </div>
     </footer>
--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -3,18 +3,46 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
+const styles = {
+  div: {
+    background: "#38a3a5",
+  },
+  brand: {
+    "font-size": "xx-large",
+    "font-weight": "bold",
+    color: "#22577a",
+    //right, down, blur, color
+    "text-shadow": "4px 4px 5px #c7f9cc",
+    "font-family": "Monoton, cursive",
+    "letter-spacing": "2px",
+  },
+  link: {
+    color: "#22577a",
+  },
+};
+
 function Navigation() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar expand="lg" style={styles.div}>
       <Container>
-        <Navbar.Brand href="/">KAMARY GILLESPIE</Navbar.Brand>
+        <Navbar.Brand href="/" style={styles.brand}>
+          KAMARY GILLESPIE
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">ABOUT ME</Nav.Link>
-            <Nav.Link href="/portfolio">MY PORTFOLIO</Nav.Link>
-            <Nav.Link href="/resume">MY RESUME</Nav.Link>
-            <Nav.Link href="/contact">CONTACT ME</Nav.Link>
+            <Nav.Link href="/" style={styles.link}>
+              ABOUT ME
+            </Nav.Link>
+            <Nav.Link href="/portfolio" style={styles.link}>
+              MY PORTFOLIO
+            </Nav.Link>
+            <Nav.Link href="/resume" style={styles.link}>
+              MY RESUME
+            </Nav.Link>
+            <Nav.Link href="/contact" style={styles.link}>
+              CONTACT ME
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
